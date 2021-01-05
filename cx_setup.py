d409724e8655a7f02b6715ea189f9a59f2b4b9b7
Arnaud Taffanel <arnaud@bitcraze.se>
2021-01-05 07:22:29 -0800

    #17: Fix Windows build
    Can build localy with Python 3.7

--- cx_setup.py
+++ cx_setup.py
@@ -15,13 +15,18 @@ files_to_copy = [
 
 setup(executables=[Executable("lpstools/gui.py", targetName="lpstool.exe",
                               base="Win32GUI", icon="bitcraze.ico")],
+      options = {
+          "build_exe": {
+              "packages": ["sip"]
+          }
+      },
       **setup_options)
 
 print()
 print("Custom copy:")
 if len(sys.argv) > 1 and sys.argv[1] == "build":
     for instr in files_to_copy:
-        destpath = "build\\exe.win32-3.6\\" + instr[1]
+        destpath = "build\\exe.win32-3.7\\" + instr[1]
         print("Making folder {}".format(destpath))
         os.makedirs(destpath, exist_ok=True)
         for filepath in glob.glob(instr[0]):
